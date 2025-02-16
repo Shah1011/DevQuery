3aa0eb3aee3c965dd94766547dd072784843a291
Shah1011 <officiallyshah@outlook.com>
2025-02-16 12:08:17 +0530

    Update connectMongoDb.js

--- server/connectMongoDb.js
+++ server/connectMongoDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.c, {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
